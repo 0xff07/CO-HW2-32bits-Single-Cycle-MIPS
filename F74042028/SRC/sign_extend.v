1cc19d6611e72200b8d67fb33fd94385a4e0a2a2
0xff <0xff@0xff.local>
2016-09-01 21:02:12 +0800

    Filing the Homework of the semester.

---
+++ F74042028/SRC/sign_extend.v
@@ -0,0 +1,11 @@
+// sign_extend
+
+module sign_extend (in, out);
+					 
+	input  [15:0] in;
+	output [31:0] out;
+	/* implement sign_extend */
+	 assign out ={{16{in[15]}},in[15:0]} ;
+  
+endmodule	
+	
